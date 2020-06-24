a2da2066ba94378963a3661efb2cdc23d52f9283
Anirudh <anirudhganwal06@gmail.com>
2020-06-24 12:20:38 +0530

    increase size on hover and comes on top

--- canvas.js
+++ canvas.js
@@ -16,39 +16,45 @@ class Navbar {
 			option.position = this.randomPointOnBoundary;
 			option.iconImage = new Image();
 			option.iconImage.src = option.icon;
+			option.speed = Math.PI / 180 * (Math.random() / 3 + 0.1);
+			option.moving = true;
+			option.radius = config.optionRadius;
 		}
 	}
 	
 	get randomPointOnBoundary() {
 		const theta = Math.random() * Math.PI * 2;
 		return {
-			x: this.radius * Math.cos(theta),
-			y: this.radius * Math.sin(theta)
+			x: this.radius * randomOffset * Math.cos(theta),
+			y: this.radius * randomOffset *  Math.sin(theta)
 		};
 	}
 	
 	getNextPosition(prevPosition, angleChange, clockwise) {
 		let theta = Math.atan2(prevPosition.y, prevPosition.x);
 		theta += clockwise ? angleChange : -angleChange;
+		let radius = Math.sqrt(Math.pow(prevPosition.x, 2) + Math.pow(prevPosition.y, 2));
 		return {
-			x: this.radius * Math.cos(theta),
-			y: this.radius * Math.sin(theta)
+			x: radius * Math.cos(theta),
+			y: radius * Math.sin(theta)
 		};
 	}
 	
 	draw = () => {
 		ctx.clearRect(-CANVAS_SIZE / 2, -CANVAS_SIZE / 2, CANVAS_SIZE, CANVAS_SIZE);
 		for (let option of this.options) {
-			const position = this.getNextPosition(option.position, Math.PI / 180 * 0.5, true);
+			const position = this.getNextPosition(option.position, option.moving ? option.speed : 0, true);
 			option.position = position;
 			ctx.lineWidth = 5;
 			ctx.strokeStyle = '#cf23c3';
+			ctx.fillStyle = '#fff';
 			ctx.beginPath();
-			ctx.arc(position.x, position.y, config.optionRadius, 0, Math.PI * 2, false);
+			ctx.arc(position.x, position.y, option.radius, 0, Math.PI * 2, false);
+			ctx.fill();
 			ctx.stroke();
 			ctx.save();
 			ctx.translate(position.x, position.y);
-			ctx.drawImage(option.iconImage, -config.optionRadius + 7, -config.optionRadius + 7, config.optionRadius * 2 - 14, config.optionRadius * 2 - 14);
+			ctx.drawImage(option.iconImage, -option.radius * 0.6, -option.radius * 0.6, option.radius * 1.2, option.radius * 1.2);
 			ctx.restore();
 		}
 	}
@@ -59,8 +65,44 @@ class Navbar {
 	}
 }
 
+const getMousePos = e => {
+	var rect = canvas.getBoundingClientRect();
+	return {
+		x: e.clientX - rect.left,
+		y: e.clientY - rect.top
+	};
+}
+
+const getDistance = (pos1, pos2) => {
+	return Math.sqrt(Math.pow(pos1.x - pos2.x, 2) + Math.pow(pos1.y - pos2.y, 2));
+}
+
+
 
 ctx.translate(CANVAS_SIZE / 2, CANVAS_SIZE / 2);
 
-const navbarCircle = new Navbar(config.navbarRadius, config.options);
-navbarCircle.animate();
+const navbar = new Navbar(config.navbarRadius, config.options);
+navbar.animate();
+
+canvas.addEventListener('mousemove', e => {
+	let mousePos = getMousePos(e);
+	let onTop = -1;
+	navbar.options.map((option, i) => {
+		if (getDistance(mousePos, {
+			x: option.position.x + CANVAS_SIZE / 2, 
+			y: option.position.y + CANVAS_SIZE / 2
+		}) < option.radius) {
+			onTop = i;
+		} else {
+			option.moving = true;
+			option.radius = config.optionRadius;
+		}
+	});
+	if (onTop !== -1) {
+		navbar.options[onTop].radius = config.optionRadius + 20;
+		navbar.options[onTop].moving = false;
+		const temp = navbar.options[onTop];
+		navbar.options.splice(onTop, 1);
+		navbar.options.push(temp);
+	}
+});
